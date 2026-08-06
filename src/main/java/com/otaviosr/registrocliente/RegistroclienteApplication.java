37153e5abe360e4b56c8b206c812cfb5c47ac65c
Otavio Sobreira <osrodrigues47@gmail.com>
2026-08-06 06:16:20 -0300

    iniciando projeto

---
+++ src/main/java/com/otaviosr/registrocliente/RegistroclienteApplication.java
@@ -0,0 +1,13 @@
+package com.otaviosr.registrocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otaviosr/registrocliente/RegistroclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otaviosr.registrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
